34b355c74b7fe956a2adf533880f9b4f26586ea3
Thippeshappa <monika.thippeshappa@accenture.com>
2019-04-09 12:21:26 +0530

    added calculator java file

---
+++ Calculator.java
@@ -0,0 +1,61 @@
+
+
+public class Calculator {
+
+	
+
+	
+
+	    public static void main(String[] args) {
+
+	    	double num1, num2;
+	        //Scanner scanner = new Scanner(System.in);
+	        //System.out.print("Enter first number:");
+
+	       // double[] argi;
+			/* We are using data type double so that user
+	         * can enter integer as well as floating point
+	         * value
+	         */
+	        num1 = Integer.parseInt(args[0]);
+	        //System.out.print("Enter second number:");
+	        num2 = Integer.parseInt(args[1]);
+
+	       // System.out.print("Enter an operator (+, -, *, /): ");
+	        String operator = args[2];
+
+	        //scanner.close();
+	        double output;
+
+	        switch(operator)
+	        {
+	            case "+":
+	            	output = num1 + num2;
+	                break;
+
+	            case "-":
+	            	output = num1 - num2;
+	                break;
+
+	            case "*":
+	            	output = num1 * num2;
+	                break;
+
+	            case "/":
+	            	output = num1 / num2;
+	                break;
+
+	            /* If user enters any other operator or char apart from
+	             * +, -, * and /, then display an error message to user
+	             * 
+	             */
+	            default:
+	                System.out.printf("You have entered wrong operator");
+	                return;
+	        }
+
+	        System.out.println(num1+" "+operator+" "+num2+": "+output);
+	    }
+	}
+
+
