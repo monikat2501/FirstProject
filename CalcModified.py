fda31c0c4fcda5b75926646e332ae86612e41875
Thippeshappa <monika.thippeshappa@accenture.com>
2019-12-27 23:02:35 +0530

    changes

--- CalcModified.py
+++ CalcModified.py
@@ -1,1 +1,42 @@
-end 0f line
+import sys
+# This function adds two numbers
+def add(x, y):
+   return x + y
+
+# This function subtracts two numbers
+def subtract(x, y):
+   return x - y
+
+# This function multiplies two numbers
+def multiply(x, y):
+   return x * y
+
+# This function divides two numbers
+def divide(x, y):
+   return x / y
+
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+# Take input from the user
+choice = sys.argv[1]
+
+num1 = int(sys.argv[2])
+num2 = int(sys.argv[3])
+
+if choice == '1':
+   print(num1,"+",num2,"=", add(num1,num2))
+
+elif choice == '2':
+   print(num1,"-",num2,"=", subtract(num1,num2))
+
+elif choice == '3':
+   print(num1,"*",num2,"=", multiply(num1,num2))
+
+elif choice == '4':
+   print(num1,"/",num2,"=", divide(num1,num2))
+else:
+   print("Invalid input")
